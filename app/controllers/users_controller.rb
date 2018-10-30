1a0d1dc139710792fd6f5405173b65978dea1fd3
papuch <ngm.etcetera@gmail.com>
2018-10-31 00:24:51 +0100

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if params[:user][:username].nil?
+      # pour le form_for
+      @user = User.new('username' => params[:name], 'email' => params[:email], 'bio' => params[:bio])
+    else
+      # pour le form_tag
+      @user = User.new('username' => params[:user][:username], 'email' => params[:user][:email], 'bio' => params[:user][:bio])
+    end
+
+    if @user.save
+      flash[:success] = "New user has been created: #{@user.username}"
+    else
+      flash[:alert] = "Couldn't create the new user as the 'Name' field is blank!"
+    end
+    redirect_to new_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'user#new'
+  get '/new', to: 'users#new'
+  post '/create', to: 'users#create'
 end
